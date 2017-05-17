2a7a5e5dd6cff235515cbfcb28225f87d7ec382c
schultzm <dr.mark.schultz@gmail.com>
2017-05-17 15:30:36 +1000

    Allowed manual filter of abricate dbs, added qacGenes db

---
+++ Pandoo/qacGenes/README.txt
@@ -0,0 +1,5 @@
+Wed 17th May, 2017
+
+This database contains a manually created list of Qac genes as these are not in any of the default databases with abricate.
+The database was created with the command:
+makeblastdb -in sequences -title qacGenes -dbtype nucl -parse_seqids -hash_index
--- pypi_registration.sh
+++ pypi_registration.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git tag 0.1.81
+git tag 0.1.82
 git push --tags
 git add .
-git commit -m 'Fixed prints to stderr'
+git commit -m 'Allowed manual filter of abricate dbs, added qacGenes db'
 git push origin -u mashtree
 python3 setup.py register -r pypitest
 python3 setup.py sdist upload -r pypitest
