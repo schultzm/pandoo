479276021f2a2f331e16bd0bd7f42d49db3c08b6
schultzm <dr.mark.schultz@gmail.com>
2017-05-17 23:33:03 +1000

    trying to merge

--- pypi_registration.sh
+++ pypi_registration.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git tag 0.1.84
+git tag 0.1.85
 git push --tags
 git add .
-git commit -m 'Fixed pandas df import bug when isolate name is an integer.'
+git commit -m 'cleaned up code blocks, ready to merge back into master'
 git push origin -u mashtree
 python3 setup.py register -r pypitest
 python3 setup.py sdist upload -r pypitest
