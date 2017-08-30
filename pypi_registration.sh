9eb989b4a984528652932a9792333f972175de1b
schultzm <dr.mark.schultz@gmail.com>
2017-08-30 10:31:25 +1000

    Fixed MLST bug

--- pypi_registration.sh
+++ pypi_registration.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cp README.md README
-git tag 0.2.01
+git tag 0.2.2
 git push --tags
 git add .
 git commit -m 'Fixed MLST bug'
 git push origin -u master
 #python3 setup.py register -r pypitest
-python3 setup.py sdist upload -r pypitest
+#python3 setup.py sdist upload -r pypitest
 #python3 setup.py register -r pypi
 python3 setup.py sdist upload -r pypi
