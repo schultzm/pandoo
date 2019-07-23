7cc349c5dd093cdd216a52c381acbc9f9042a9fd
Mark Schultz <dr.mark.schultz@gmail.com>
2019-07-23 11:12:36 +1000

    Update __init__.py

--- Pandoo/__init__.py
+++ Pandoo/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __name__            = 'paando'
-__version__         = '0.2.25'
-__version_date__    = 'Mon 6th August, 2018'
+__version__         = '0.2.26'
+__version_date__    = 'Tue 23rd August, 2019 (Miller is in town)'
 __description__     = ('Pandoo is a bioinformatics tool for for ' +\
                        'exploring and characterising bacterial genome ' +\
                        'data.  Input is paired-end reads and assemblies.  ' +\
